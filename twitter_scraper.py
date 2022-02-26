925a243f18500afeff763ad3494b8ae01f95f975
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-02-25 22:41:54 -0500

    Add files via upload
    Added error handling for invalid tokens or username

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -11,17 +11,22 @@ bearer_token = ''
 #Grabbing tweets!
 username = input("Enter the twitter handle of the user you want to converse with: ")
 client = tweepy.Client(bearer_token=bearer_token)
-users = client.get_user(username=username.strip('@'), user_fields=['id'])
-userID = users[0]["id"]
+try:
+    users = client.get_user(username=username.strip('@'), user_fields=['id'])
+    userID = users[0]["id"]
+    # Scraping set
+    training_set = client.get_users_tweets(id=userID, max_results=100)
 
-# Scraping set
-training_set = client.get_users_tweets(id=userID, max_results=100)
+    # Write to text file
+    os.remove("trainingfile.txt")
+    txtfile = open("trainingfile.txt", "a")
+    for tweet in training_set.data:
+        txtfile.write(str(tweet)+'\n')
+except:
+    print("Invalid User or Invalid developer token")
+    exit()
 
-# Write to text file
-os.remove("trainingfile.txt")
-txtfile = open("trainingfile.txt", "a")
-for tweet in training_set.data:
-    txtfile.write(str(tweet)+'\n')
+
 
 
 
